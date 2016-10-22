76d355f2862d6c435278fd8e3d2b800e0f68cc17
Mark Provan <mrp4@st-andrews.ac.uk>
2016-10-22 17:08:43 +0100

    Clear up project directory

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-tsc src/*.ts --outDir gen
-browserify gen/script.js -o shwww/bundle.js
-cp src/index.html shwww/index.html
-node_modules/node-sass/bin/node-sass src/styles.scss > shwww/styles.css
