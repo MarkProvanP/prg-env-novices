4a5e59b6080f05b8027b35d5df4d12fb3f379876
Mark Provan <mrp4@st-andrews.ac.uk>
2016-11-21 21:22:26 +0000

    Implement most of machine model

--- pegjs-test/test-peg.js
+++ pegjs-test/test-peg.js
@@ -31,115 +31,265 @@ function callCodegen(thing) {
         decTabs();
     }
 }
+var StackElement = (function () {
+    function StackElement() {
+    }
+    return StackElement;
+}());
+var EnvElement = (function () {
+    function EnvElement() {
+    }
+    EnvElement.prototype.get = function (key) {
+        return this.mapping[key];
+    };
+    EnvElement.prototype.set = function (key, value) {
+        this.mapping[key] = value;
+    };
+    EnvElement.prototype.hasKey = function (key) {
+        return this.mapping.hasOwnProperty(key);
+    };
+    return EnvElement;
+}());
+var EnvChange = (function () {
+    function EnvChange(key, before, after, envNo) {
+        this.key = key;
+    }
+    return EnvChange;
+}());
 var Machine = (function () {
     function Machine(instructions) {
         var _this = this;
         this.instructions = instructions;
         this.stack = [];
-        this.env = {};
+        this.envStack = [];
         this.instructionPointer = 0;
         this.labelMap = {};
         this.instructions.forEach(function (instruction, index) {
-            if (instruction instanceof LabelInstruction) {
-                _this.labelMap[instruction.label] = index;
+            if (instruction instanceof Label) {
+                var labelInstruction = instruction;
+                _this.labelMap[labelInstruction.label] = index;
             }
         });
     }
+    Machine.prototype.peek = function (n) {
+        if (typeof (n) == 'number') {
+            return this.stack.slice(0, n);
+        }
+        return this.stack[this.stack.length - 1];
+    };
+    Machine.prototype.peekEnv = function () {
+        return this.envStack[this.envStack.length - 1];
+    };
+    Machine.isTruthy = function (val) {
+        return !!val;
+    };
+    Machine.prototype.getIndexOfEnvWithKey = function (key) {
+        for (var i = 0; i < this.envStack.length; i++) {
+            var env = this.envStack[i];
+            if (env.hasKey(key)) {
+                return i;
+            }
+        }
+    };
     return Machine;
 }());
+var MachineChange = (function () {
+    function MachineChange(stackPushed, stackPopped, envPushed, envPopped, envChanged, ipChange) {
+        this.stackPushed = stackPushed;
+        this.stackPopped = stackPopped;
+        this.envPushed = envPushed;
+        this.envPopped = envPopped;
+        this.envChanged = envChanged;
+        this.ipChange = ipChange;
+    }
+    MachineChange.create = function (_a) {
+        var _b = _a.stackPushed, stackPushed = _b === void 0 ? undefined : _b, _c = _a.stackPopped, stackPopped = _c === void 0 ? undefined : _c, _d = _a.envPushed, envPushed = _d === void 0 ? undefined : _d, _e = _a.envPopped, envPopped = _e === void 0 ? undefined : _e, _f = _a.envChanged, envChanged = _f === void 0 ? undefined : _f, _g = _a.ipChange, ipChange = _g === void 0 ? undefined : _g;
+        return new MachineChange(stackPushed, stackPopped, envPushed, envPopped, envChanged, ipChange);
+    };
+    return MachineChange;
+}());
+var MachineFunction = (function () {
+    function MachineFunction(name, arity, code) {
+        this.name = name;
+        this.arity = arity;
+        this.code = code;
+    }
+    MachineFunction.prototype.apply = function (fThis, fArgs) {
+        return this.code.apply(fThis, fArgs);
+    };
+    return MachineFunction;
+}());
+var builtInFunctions = {
+    '+': new MachineFunction('add', 2, function (a, b) { return a + b; }),
+    '-': new MachineFunction('subtract', 2, function (a, b) { return a - b; }),
+    '*': new MachineFunction('multiply', 2, function (a, b) { return a * b; }),
+    '/': new MachineFunction('divide', 2, function (a, b) { return a / b; }),
+    '!': new MachineFunction('not', 1, function (a) { return !a; })
+};
 var Instruction = (function () {
     function Instruction() {
     }
     return Instruction;
 }());
-var PushInstruction = (function (_super) {
-    __extends(PushInstruction, _super);
-    function PushInstruction(val) {
+var Push = (function (_super) {
+    __extends(Push, _super);
+    function Push(val) {
         _super.call(this);
         this.val = val;
     }
-    return PushInstruction;
+    Push.prototype.machineChange = function (machine) {
+        return MachineChange.create({ stackPushed: this.val, ipChange: 1 });
+    };
+    return Push;
 }(Instruction));
-var BinOpInstruction = (function (_super) {
-    __extends(BinOpInstruction, _super);
-    function BinOpInstruction(op) {
+var Pop = (function (_super) {
+    __extends(Pop, _super);
+    function Pop() {
         _super.call(this);
-        this.op = op;
     }
-    return BinOpInstruction;
+    Pop.prototype.machineChange = function (machine) {
+        return MachineChange.create({ stackPopped: machine.peek(), ipChange: 1 });
+    };
+    return Pop;
 }(Instruction));
-var IfGotoInstruction = (function (_super) {
-    __extends(IfGotoInstruction, _super);
-    function IfGotoInstruction(label) {
+var Dup = (function (_super) {
+    __extends(Dup, _super);
+    function Dup() {
+        _super.call(this);
+    }
+    Dup.prototype.machineChange = function (machine) {
+        return MachineChange.create({ stackPushed: machine.peek(), ipChange: 1 });
+    };
+    return Dup;
+}(Instruction));
+var NewEnv = (function (_super) {
+    __extends(NewEnv, _super);
+    function NewEnv() {
+        _super.call(this);
+    }
+    NewEnv.prototype.machineChange = function (machine) {
+        return MachineChange.create({ envPushed: new EnvElement(), ipChange: 1 });
+    };
+    return NewEnv;
+}(Instruction));
+var PopEnv = (function (_super) {
+    __extends(PopEnv, _super);
+    function PopEnv() {
+        _super.call(this);
+    }
+    PopEnv.prototype.machineChange = function (machine) {
+        return MachineChange.create({ envPopped: machine.peekEnv(), ipChange: 1 });
+    };
+    return PopEnv;
+}(Instruction));
+var CallFunction = (function (_super) {
+    __extends(CallFunction, _super);
+    function CallFunction(func) {
+        _super.call(this);
+        this.func = func;
+    }
+    CallFunction.prototype.machineChange = function (machine) {
+        var arity = this.func.arity;
+        var args = machine.peek(arity);
+        var popped = [this.func].concat(args);
+        var pushed = [this.func.apply(null, args)];
+        return MachineChange.create({ stackPushed: pushed, stackPopped: popped, ipChange: 1 });
+    };
+    return CallFunction;
+}(Instruction));
+var IfGoto = (function (_super) {
+    __extends(IfGoto, _super);
+    function IfGoto(label) {
         _super.call(this);
         this.label = label;
     }
-    return IfGotoInstruction;
+    IfGoto.prototype.machineChange = function (machine) {
+        var stackTop = machine.peek();
+        var isTruthy = Machine.isTruthy(stackTop);
+        if (isTruthy) {
+            var originalIP = machine.instructionPointer;
+            var newIP = machine.labelMap[this.label];
+            var change = newIP - originalIP;
+            return MachineChange.create({ stackPopped: stackTop, ipChange: change });
+        }
+        else {
+            return MachineChange.create({ stackPopped: stackTop, ipChange: 1 });
+        }
+    };
+    return IfGoto;
 }(Instruction));
-var LabelInstruction = (function (_super) {
-    __extends(LabelInstruction, _super);
-    function LabelInstruction(label) {
+var Label = (function (_super) {
+    __extends(Label, _super);
+    function Label(label) {
         _super.call(this);
         this.label = label;
     }
-    return LabelInstruction;
+    Label.prototype.machineChange = function (machine) {
+        return MachineChange.create({ ipChange: 1 });
+    };
+    return Label;
 }(Instruction));
-var UnOpInstruction = (function (_super) {
-    __extends(UnOpInstruction, _super);
-    function UnOpInstruction(op) {
-        _super.call(this);
-        this.op = op;
-    }
-    return UnOpInstruction;
-}(Instruction));
-var SetInstruction = (function (_super) {
-    __extends(SetInstruction, _super);
-    function SetInstruction(key) {
-        _super.call(this);
-        this.key = key;
-        console.log(key);
-    }
-    return SetInstruction;
-}(Instruction));
-var GetInstruction = (function (_super) {
-    __extends(GetInstruction, _super);
-    function GetInstruction(key) {
+var Set = (function (_super) {
+    __extends(Set, _super);
+    function Set(key) {
         _super.call(this);
         this.key = key;
     }
-    return GetInstruction;
+    Set.prototype.machineChange = function (machine) {
+        var index = machine.getIndexOfEnvWithKey(this.key);
+        var env = machine.envStack[index];
+        var before = env.get(this.key);
+        var value = machine.peek();
+        var envChanged = new EnvChange(this.key, before, value, index);
+        return MachineChange.create({ stackPopped: value, envChanged: envChanged, ipChange: 1 });
+    };
+    return Set;
+}(Instruction));
+var Get = (function (_super) {
+    __extends(Get, _super);
+    function Get(key) {
+        _super.call(this);
+        this.key = key;
+    }
+    Get.prototype.machineChange = function (machine) {
+        var index = machine.getIndexOfEnvWithKey(this.key);
+        var env = machine.envStack[index];
+        var pushed = env.get(this.key);
+        return MachineChange.create({ stackPushed: pushed, ipChange: 1 });
+    };
+    return Get;
 }(Instruction));
 var codegens = {
     Integer: function (i) {
         operation("push " + i.value);
-        addInstruction(new PushInstruction(i.value));
+        addInstruction(new Push(i.value));
     },
     BinaryExpression: function (e) {
         callCodegen(e.left);
         callCodegen(e.right);
         operation(e.op);
-        addInstruction(new BinOpInstruction(e.op));
+        addInstruction(new CallFunction(builtInFunctions[e.op]));
     },
     AssignmentStatement: function (s) {
         callCodegen(s.expression);
         operation('set ' + s.ident.name);
-        addInstruction(new SetInstruction(s.ident.name));
+        addInstruction(new Set(s.ident.name));
     },
     WhileStatement: function (s) {
         var whileBeginLabel = "whileBegin";
         var whileEndLabel = "whileEnd";
         operation('LABEL begin');
-        addInstruction(new LabelInstruction(whileBeginLabel));
+        addInstruction(new Label(whileBeginLabel));
         callCodegen(s.condition);
         operation('if not goto end');
-        addInstruction(new UnOpInstruction("!"));
-        addInstruction(new IfGotoInstruction(whileEndLabel));
+        addInstruction(new CallFunction(builtInFunctions['!']));
+        addInstruction(new IfGoto(whileEndLabel));
         s.statements.forEach(function (statement) { return callCodegen(statement); });
         operation('goto begin');
-        addInstruction(new PushInstruction(1));
-        addInstruction(new IfGotoInstruction(whileBeginLabel));
+        addInstruction(new Push(1));
+        addInstruction(new IfGoto(whileBeginLabel));
         operation('LABEL end');
-        addInstruction(new LabelInstruction(whileEndLabel));
+        addInstruction(new Label(whileEndLabel));
     }
 };
 var instructions = [];
--- pegjs-test/test-peg.ts
+++ pegjs-test/test-peg.ts
@@ -36,100 +36,264 @@ function callCodegen(thing) {
   }
 }
 
-class Machine {
-  stack = [];
-  env = {};
+class StackElement {
 
-  instructionPointer = 0;
-  labelMap = {};
+}
+
+class EnvElement {
+  mapping: {};
+
+  public get(key: string) {
+    return this.mapping[key];
+  }
+
+  public set(key: string, value: any) {
+    this.mapping[key] = value;
+  }
+
+  hasKey(key: string) {
+    return this.mapping.hasOwnProperty(key);
+  }
+}
+
+class EnvChange {
+  constructor(public key: string, before: any, after: any, envNo: number) {}
+}
+
+class Machine {
+  public stack = [];
+  public envStack = []
+
+  public instructionPointer = 0;
+  public labelMap = {};
 
   constructor(private instructions: Instruction[]) {
     this.instructions.forEach((instruction, index) => {
-      if (instruction instanceof LabelInstruction) {
-        this.labelMap[instruction.label] = index;
+      if (instruction instanceof Label) {
+        let labelInstruction = <Label> instruction;
+        this.labelMap[labelInstruction.label] = index;
       }
     })
   }
+
+  peek(n?: number) {
+    if (typeof(n) == 'number') {
+      return this.stack.slice(0, n)
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  peekEnv() {
+    return this.envStack[this.envStack.length - 1];
+  }
+
+  static isTruthy(val) {
+    return !!val;
+  }
+
+  getIndexOfEnvWithKey(key: string) {
+    for (let i = 0; i < this.envStack.length; i++) {
+      let env = this.envStack[i];
+      if (env.hasKey(key)) {
+        return i;
+      }
+    }
+  }
+}
+
+class MachineChange {
+  constructor(
+    private stackPushed: StackElement[],
+    private stackPopped: StackElement[],
+    private envPushed: EnvElement[],
+    private envPopped: EnvElement[],
+    private envChanged: EnvChange,
+    private ipChange: number
+  ) {
+
+  }
+
+  static create({stackPushed = undefined, stackPopped = undefined, envPushed = undefined, envPopped = undefined, envChanged = undefined, ipChange = undefined}) {
+    return new MachineChange(stackPushed, stackPopped, envPushed, envPopped, envChanged, ipChange);
+  }
+}
+
+class MachineFunction {
+  constructor(
+    public name: string,
+    public arity: number,
+    private code
+  ) {}
+
+  apply(fThis, fArgs) {
+    return this.code.apply(fThis, fArgs)
+  }
+}
+
+let builtInFunctions = {
+  '+': new MachineFunction('add', 2, (a, b) => a + b),
+  '-': new MachineFunction('subtract', 2, (a, b) => a - b),
+  '*': new MachineFunction('multiply', 2, (a, b) => a * b),
+  '/': new MachineFunction('divide', 2, (a, b) => a / b),
+  '!': new MachineFunction('not', 1, (a) => !a)
 }
 
 abstract class Instruction {
-
+  abstract machineChange(machine: Machine): MachineChange;
 }
 
-class PushInstruction extends Instruction {
-  constructor(public val) {
+class Push extends Instruction {
+  constructor(public val: StackElement) {
     super()
+  }
+
+  machineChange(machine: Machine) {
+    return MachineChange.create({stackPushed: this.val, ipChange: 1})
   }
 }
 
-class BinOpInstruction extends Instruction {
-  constructor(public op) {
+class Pop extends Instruction {
+  constructor() {
     super()
+  }
+
+  machineChange(machine: Machine) {
+    return MachineChange.create({stackPopped: machine.peek(), ipChange: 1})
   }
 }
 
-class IfGotoInstruction extends Instruction {
+class Dup extends Instruction {
+  constructor() {
+    super()
+  }
+
+  machineChange(machine: Machine) {
+    return MachineChange.create({stackPushed: machine.peek(), ipChange: 1})
+  }
+}
+
+class NewEnv extends Instruction {
+  constructor() {
+    super()
+  }
+
+  machineChange(machine: Machine) {
+    return MachineChange.create({envPushed: new EnvElement(), ipChange: 1})
+  }
+}
+
+class PopEnv extends Instruction {
+  constructor() {
+    super()
+  }
+
+  machineChange(machine: Machine) {
+    return MachineChange.create({envPopped: machine.peekEnv(), ipChange: 1})
+  }
+}
+
+class CallFunction extends Instruction {
+  constructor(private func: MachineFunction) {
+    super()
+  }
+
+  machineChange(machine: Machine) {
+    let arity = this.func.arity;
+    let args = machine.peek(arity);
+    let popped = [this.func].concat(args)
+    let pushed = [this.func.apply(null, args)]
+    return MachineChange.create({stackPushed: pushed, stackPopped: popped, ipChange: 1})
+  }
+}
+
+class IfGoto extends Instruction {
   constructor(public label) {
     super();
   }
+
+  machineChange(machine: Machine) {
+    let stackTop = machine.peek();
+    let isTruthy = Machine.isTruthy(stackTop);
+    if (isTruthy) {
+      let originalIP = machine.instructionPointer;
+      let newIP = machine.labelMap[this.label];
+      let change = newIP - originalIP;
+      return MachineChange.create({stackPopped: stackTop, ipChange: change})
+    } else {
+      return MachineChange.create({stackPopped: stackTop, ipChange: 1})
+    }
+  }
 }
 
-class LabelInstruction extends Instruction {
+class Label extends Instruction {
   constructor(public label) {
     super()
   }
-}
 
-class UnOpInstruction extends Instruction {
-  constructor(public op) {
-    super()
+  machineChange(machine: Machine) {
+    return MachineChange.create({ipChange: 1})
   }
 }
 
-class SetInstruction extends Instruction {
+class Set extends Instruction {
   constructor(public key) {
     super();
-    console.log(key);
+  }
+
+  machineChange(machine: Machine) {
+    let index = machine.getIndexOfEnvWithKey(this.key);
+    let env = machine.envStack[index];
+    let before = env.get(this.key);
+    let value = machine.peek();
+    let envChanged = new EnvChange(this.key, before, value, index);
+    return MachineChange.create({stackPopped: value, envChanged: envChanged, ipChange: 1})
   }
 }
 
-class GetInstruction extends Instruction {
+class Get extends Instruction {
   constructor(public key) {
     super();
+  }
+
+  machineChange(machine: Machine) {
+    let index = machine.getIndexOfEnvWithKey(this.key);
+    let env = machine.envStack[index];
+    let pushed = env.get(this.key);
+    return MachineChange.create({stackPushed: pushed, ipChange: 1})
   }
 }
 
 let codegens = {
   Integer: function(i) {
     operation("push " + i.value);
-    addInstruction(new PushInstruction(i.value))
+    addInstruction(new Push(i.value))
   },
   BinaryExpression: function(e) {
     callCodegen(e.left);
     callCodegen(e.right);
     operation(e.op);
-    addInstruction(new BinOpInstruction(e.op));
+    addInstruction(new CallFunction(builtInFunctions[e.op]))
   },
   AssignmentStatement: function(s) {
     callCodegen(s.expression);
     operation('set ' + s.ident.name);
-    addInstruction(new SetInstruction(s.ident.name));
+    addInstruction(new Set(s.ident.name));
   },
   WhileStatement: function(s) {
     let whileBeginLabel = "whileBegin";
     let whileEndLabel = "whileEnd";
     operation('LABEL begin')
-    addInstruction(new LabelInstruction(whileBeginLabel))
+    addInstruction(new Label(whileBeginLabel))
     callCodegen(s.condition);
     operation('if not goto end');
-    addInstruction(new UnOpInstruction("!"));
-    addInstruction(new IfGotoInstruction(whileEndLabel))
+    addInstruction(new CallFunction(builtInFunctions['!']))
+    addInstruction(new IfGoto(whileEndLabel))
     s.statements.forEach((statement) => callCodegen(statement));
     operation('goto begin')
-    addInstruction(new PushInstruction(1))
-    addInstruction(new IfGotoInstruction(whileBeginLabel));
+    addInstruction(new Push(1))
+    addInstruction(new IfGoto(whileBeginLabel));
     operation('LABEL end');
-    addInstruction(new LabelInstruction(whileEndLabel))
+    addInstruction(new Label(whileEndLabel))
   }
 }
 
