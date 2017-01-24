f0786af49f488b8aea30c72a059e6c1b0f7ee577
Mark Provan <mrp4@st-andrews.ac.uk>
2017-01-24 17:16:25 +0000

    Target ES6, then babelify

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.ts$/, loader: 'babel-loader!ts-loader' },
       { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
       { test: /\.peg$/, loader: 'raw-loader'}
     ]
